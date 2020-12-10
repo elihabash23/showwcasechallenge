3a2e4a3a64218545a1a36e930133cec499e8330a
Elias Habash <eliashabash23@yahoo.com>
2020-12-10 02:40:01 -0800

    Added basic layout for components: Education, EducationModal, SidePanel. Also made small changes to MainScreen Page

--- src/pages/MainScreen/MainScreen.tsx
+++ src/pages/MainScreen/MainScreen.tsx
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import Button from '../../components/UI/Button/Button';
 import { getName, getEducation } from '../../store/reducers/User';
 
 const MainScreen = () => {
+	const [showEducationModal, setShowEducationModal] = useState(false);
 	const name = useSelector(getName);
 	const education = useSelector(getEducation);
+	const history = useHistory();
+
+	useEffect(() => {
+		if (!name) {
+			history.push("/");
+		}
+	})
 
 	return (
 		<div>
-			
+			<h1>Welcome to {name}'s education page</h1>
+			<Button
+				type="button"
+				onClick={() => setShowEducationModal(true)}>
+				Add new education
+			</Button>
 		</div>
 	);
 }
